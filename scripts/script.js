05112fcaa9f706733ff7b4716174af4385b86a48
Samael <samael803@gmail.com>
2019-03-13 15:14:12 +0200

    refactors initContent and adds default poster for missing items

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 // Variables
 const searchBtn = document.querySelector('button');
 const results = document.querySelector('ul');
+const input = document.querySelector('input');
 let pageNum; // to be used for sending request for the items in the next page (if exists)
 
 // Event Listeners
@@ -10,8 +11,7 @@ searchBtn.addEventListener('click', handleSearch);
 function handleSearch() {
 
     // to do: validation that the search term is at least 3 characters long
-    const searchTerm = document.querySelector('input').value;
-    const endpoint = `http://www.omdbapi.com/?apikey=d777cf78&s=${searchTerm}&type=movie&page=${pageNum || 1}`;
+    const endpoint = `http://www.omdbapi.com/?apikey=d777cf78&s=${input.value}&type=movie&page=${pageNum || 1}`;
 
     // to do: error handling
     fetch(endpoint)
@@ -20,29 +20,35 @@ function handleSearch() {
 
 }
 
-function initContent(blob) {
+function initContent(movies) {
 
-    results.innerHTML = ''; // reset content
+    // Reset content
+    results.innerHTML = '';
 
-    if (blob.Response == 'False') {
+    // Stop function if search failed
+    if (movies.Response == 'False') {
         console.log(`The movie you are looking for does not exist.`);
         return;
     }
 
-    const pages = Math.ceil(blob.totalResults/10);
-    // to do: conditional to disable 'load more results' when last page is loaded (btn not yet added)
+    // Number of total pages
+    const pages = Math.ceil(movies.totalResults/10);
 
-    for (let movie of blob.Search) {
-        // to do: default image for the case of missing movie poster
-        // to do: move into a separate function
+    // Show first page
+    for (let movie of movies.Search) { // to do: move into a separate function
         let li = document.createElement('li');
+        li.innerHTML = `<img src="${(movie.Poster == 'N/A') ? 'images/default_poster.jpg' : movie.Poster}" height="75"> ${movie.Title}`;
         li.dataset.imdbid = movie.imdbID;
-        results.appendChild(li).innerHTML = `<img src="${movie.Poster}" height="75"> ${movie.Title}`; // <= REFACTOR
+        results.appendChild(li);
     }
 
+    // Fetch next Page    
+
+    // to do: conditional to disable 'load more results' when last page is loaded (btn not yet added)
+
     // debugging
-    console.table(blob.Search);
-    console.log(blob);
+    console.table(movies.Search);
+    console.log(movies);
     console.log(`There are ${pages} pages for this query.`);
 
 }
